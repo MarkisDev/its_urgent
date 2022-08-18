b323baff76a3415a2fa0d7875656eab9fba0b1fb
Nitin Sharma <76255199+nitin-787@users.noreply.github.com>
2022-08-18 22:54:08 +0530

    Bottom bar ui + code (#27)
    * Bottom bar ui + code
    
    * Removed icon plus dependency
    
    * Update navbar.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
-import 'package:its_urgent/screens/login/index.dart';
+import 'package:its_urgent/utils/navbar.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
-
 import 'provider/login.dart';
-import 'screens/contacts.dart';
-import 'screens/dialer.dart';
-import 'screens/settings/index.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,57 +26,8 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.light,
           primarySwatch: Colors.green,
         ),
-        home: const NavigationExample(),
+        home: const BottomNavigation(),
       ),
-    );
-  }
-}
-
-class NavigationExample extends StatefulWidget {
-  const NavigationExample({Key? key}) : super(key: key);
-
-  @override
-  State<NavigationExample> createState() => _NavigationExampleState();
-}
-
-class _NavigationExampleState extends State<NavigationExample> {
-  int currentPageIndex = 1;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        selectedIndex: currentPageIndex,
-        destinations: const <Widget>[
-          NavigationDestination(
-            icon: Icon(Icons.contacts),
-            label: 'Contacts',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.dialpad),
-            label: 'Dialer',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.login),
-            label: 'Login',
-          ),
-        ],
-      ),
-      body: <Widget>[
-        const ContactPage(),
-        const DialerPage(),
-        const SettingPage(),
-        const LoginPage(),
-      ][currentPageIndex],
     );
   }
 }
---
+++ lib/utils/navbar.dart
@@ -0,0 +1,77 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:its_urgent/constants.dart';
+import 'package:its_urgent/screens/contacts.dart';
+import 'package:its_urgent/screens/dialer.dart';
+import 'package:its_urgent/screens/login/index.dart';
+import 'package:its_urgent/screens/settings/index.dart';
+
+class BottomNavigation extends StatefulWidget {
+  const BottomNavigation({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavigation> createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  int currentPageIndex = 0;
+
+  // icons for the bottom navigation bar
+  final List<Widget> _icons = [
+    const Icon(Icons.phone),
+    const Icon(Icons.contacts),
+    const Icon(Icons.settings),
+    const Icon(Icons.person),
+  ];
+
+  // index for navigate to the page
+  int currentTab = 0;
+
+  // pages
+  final List<Widget> pages = [
+    const ContactPage(),
+    const DialerPage(),
+    const SettingPage(),
+    const LoginPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        bottomNavigationBar: CurvedNavigationBar(
+          items: _icons,
+          index: currentTab,
+          height: 50.0,
+          color: kPrimaryColor,
+          backgroundColor: Colors.transparent,
+          animationCurve: Curves.easeInOut,
+          onTap: (index) {
+            setState(() {
+              currentTab = index;
+            });
+          },
+        ),
+        body: getSelectedWidget(index: currentTab));
+  }
+
+  Widget getSelectedWidget({required int index}) {
+    Widget widget;
+    switch (index) {
+      case 0:
+        widget = const DialerPage();
+        break;
+      case 1:
+        widget = const ContactPage();
+        break;
+      case 2:
+        widget = const SettingPage();
+        break;
+      case 3:
+        widget = const LoginPage();
+        break;
+      default:
+        widget = const DialerPage();
+    }
+    return widget;
+  }
+}
